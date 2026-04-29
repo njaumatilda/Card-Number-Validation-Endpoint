6f809fb30ad2d4a9ff548d94d08700fa0250addd
Matilda-Njau <matildawanjirunjau@gmail.com>
2026-04-29 18:57:33 +0300

    feat: add endpoint to test live url

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use('/api', cardValidationRoutes)
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Card Number Validation API!')
+})
+
 app.listen(PORT, () => {
   console.log(`[server]: App listening on port: ${PORT}`)
 })
