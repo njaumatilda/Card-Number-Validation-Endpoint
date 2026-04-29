a5ec4988a2ad4676f01453888e40ed1c6f47768d
Matilda-Njau <matildawanjirunjau@gmail.com>
2026-04-29 10:31:54 +0300

    feat: setup card validation routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 
+import cardValidationRoutes from './routes/cardValidation.js'
+
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use('/api', cardValidationRoutes)
 
 app.listen(PORT, () => {
   console.log(`[server]: App listening on port: ${PORT}`)
---
+++ src/controllers/cardValidation.ts
@@ -0,0 +1,3 @@
+const validateCardNumber = () => {}
+
+export { validateCardNumber }
