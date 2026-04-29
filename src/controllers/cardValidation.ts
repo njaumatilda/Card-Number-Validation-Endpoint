34b9d7cfdef99845e267d7b083276434c7b1d5ba
Matilda-Njau <matildawanjirunjau@gmail.com>
2026-04-29 12:58:43 +0300

    feat: implement success response for valid card numbers in controller

--- src/controllers/cardValidation.ts
+++ src/controllers/cardValidation.ts
@@ -1,3 +1,7 @@
-const validateCardNumber = () => {}
+import type { Request, Response } from 'express'
+
+const validateCardNumber = (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Card number is valid' })
+}
 
 export { validateCardNumber }
--- src/routes/cardValidation.ts
+++ src/routes/cardValidation.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { validateCardNumber } from '../controllers/cardValidation.js'
+import cardValidator from '../middlewares/cardValidation.js'
 
 const router: Router = Router()
 
-router.post('/validate-card', validateCardNumber)
+router.post('/validate-card', cardValidator, validateCardNumber)
 
 export default router
